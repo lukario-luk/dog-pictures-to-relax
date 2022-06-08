1342cf042d7382fb3e87ab82a823688389dab2b5
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-06-08 18:05:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
+import "./App.css"
+export default function DogButton () {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [dog, setDog] = useState([])
+const [DisplayImage, setDisplayImage] = useState(false)
+
+const dogList = ()=>{
+    try{
+      axios.get(`https://dog.ceo/api/breeds/image/random`).then(response => {
+        setDog(response.data.message)
+        setDisplayImage(true)
+      }) 
+    }catch(error){
+      console.log("Tratar erro:", error)
+    }
 }
+    return (
+        <>
+            <section id="container-btn-dog">
+                <div>
+                  <button onClick={() =>{dogList()}}><b>dog pictures to relax</b></button>
+                </div>
+                <figure>
+                  {DisplayImage && (<img id="dog-img" onClick={() =>{dogList()}} src={dog} alt="imagem de cachorro aleatoria"/>)}  
+                </figure>
 
-export default App;
+            </section>
+        </>
+    )
+}
